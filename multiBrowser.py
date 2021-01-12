4ed3fc33167e279c1885874d20f02e369a7af0d5
shanatm1 <shanatm310@gmail.com>
2021-01-12 23:11:42 +0100

    small updates

--- multiBrowser.py
+++ multiBrowser.py
@@ -4,4 +4,4 @@ from selenium.webdriver.common.keys import Keys
 driver=webdriver.Chrome(executable_path="C:/Drivers/chromedriver_win32/chromedriver.exe")
 driver.get("http://newtours.demoaut.com/")
 print(driver.title)
-##driver.close()
+driver.close()
